2373384ce30494d3f1c0adfd1ac6acb855f657e4
Chris Huang <chrishuang128@gmail.com>
2019-05-22 01:01:34 -0700

    Implemented basic search bar appearence

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -6,7 +6,9 @@ class Search extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			party: 2
+			party: 2,
+			time: '7:00 PM',
+			date: 'May 22, 2019'
 		};
 		this.timeSlotRender = this.timeSlotRender.bind(this);
 	}
@@ -17,11 +19,7 @@ class Search extends React.Component {
 		return (
 			<div>
 				<div className="close-button">
-					<a
-						href="javascript:void(0)"
-						data-target="#header-search-box"
-						className="js-toggle-search"
-					>
+					<a data-target="#header-search-box" className="js-toggle-search">
 						<i className="icon-close" />
 					</a>
 				</div>
@@ -64,8 +62,87 @@ class Search extends React.Component {
 									<option value="21">Large Party</option>
 								</select>
 							</div>
-							<div />
-							<div />
+							<div className="date-container">
+								<a className="dtp-picker-selector-link date-label dtp-picker-label">
+									May 22, 2019
+								</a>
+								<input
+									type="text"
+									title="datepicker"
+									name="datepicker"
+									className="datepicker dtp-picker-select picker__input"
+									data-value="2019-05-22"
+									readOnly=""
+									id="P1932029059"
+									aria-haspopup="true"
+									aria-expanded="false"
+									aria-readonly="false"
+									aria-owns="P1932029059_root submit_datepicker"
+									aria-label="date"
+								/>
+								<div />
+							</div>
+							<div className="time-container">
+								<a
+									className="select-label dtp-picker-selector-link"
+									tabIndex="-1"
+								>
+									{this.state.time}
+								</a>
+								<select
+									className="time-selector"
+									name="Select_0"
+									aria-label="time"
+								>
+									<option value="00:30">12:30 AM</option>
+									<option value="01:00">1:00 AM</option>
+									<option value="01:30">1:30 AM</option>
+									<option value="02:00">2:00 AM</option>
+									<option value="02:30">2:30 AM</option>
+									<option value="03:00">3:00 AM</option>
+									<option value="03:30">3:30 AM</option>
+									<option value="04:00">4:00 AM</option>
+									<option value="04:30">4:30 AM</option>
+									<option value="05:00">5:00 AM</option>
+									<option value="05:30">5:30 AM</option>
+									<option value="06:00">6:00 AM</option>
+									<option value="06:30">6:30 AM</option>
+									<option value="07:00">7:00 AM</option>
+									<option value="07:30">7:30 AM</option>
+									<option value="08:00">8:00 AM</option>
+									<option value="08:30">8:30 AM</option>
+									<option value="09:00">9:00 AM</option>
+									<option value="09:30">9:30 AM</option>
+									<option value="10:00">10:00 AM</option>
+									<option value="10:30">10:30 AM</option>
+									<option value="11:00">11:00 AM</option>
+									<option value="11:30">11:30 AM</option>
+									<option value="12:00">12:00 PM</option>
+									<option value="12:30">12:30 PM</option>
+									<option value="13:00">1:00 PM</option>
+									<option value="13:30">1:30 PM</option>
+									<option value="14:00">2:00 PM</option>
+									<option value="14:30">2:30 PM</option>
+									<option value="15:00">3:00 PM</option>
+									<option value="15:30">3:30 PM</option>
+									<option value="16:00">4:00 PM</option>
+									<option value="16:30">4:30 PM</option>
+									<option value="17:00">5:00 PM</option>
+									<option value="17:30">5:30 PM</option>
+									<option value="18:00">6:00 PM</option>
+									<option value="18:30">6:30 PM</option>
+									<option value="19:00">7:00 PM</option>
+									<option value="19:30">7:30 PM</option>
+									<option value="20:00">8:00 PM</option>
+									<option value="20:30">8:30 PM</option>
+									<option value="21:00">9:00 PM</option>
+									<option value="21:30">9:30 PM</option>
+									<option value="22:00">10:00 PM</option>
+									<option value="22:30">10:30 PM</option>
+									<option value="23:00">11:00 PM</option>
+									<option value="23:30">11:30 PM</option>
+								</select>
+							</div>
 						</div>
 						<div className="picker-search-container">
 							<div className="search-icon" />
