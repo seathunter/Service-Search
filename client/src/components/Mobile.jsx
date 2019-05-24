2094003da7d94b95fe4bde2894ccbdf4b3d38a42
Chris Huang <chrishuang128@gmail.com>
2019-05-24 12:29:49 -0700

    implemente array for dropdown menu for mobile

--- client/src/components/Mobile.jsx
+++ client/src/components/Mobile.jsx
@@ -5,14 +5,15 @@ class Mobile extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			expand: false
+			expand: false,
+			mobile: false
 		};
 		this.clickHandler = this.clickHandler.bind(this);
 	}
 
 	clickHandler(e) {
 		e.preventDefault();
-		this.setState({ expand: !this.state.expand });
+		this.setState({ expand: !this.state.expand, mobile: !this.state.mobile });
 	}
 
 	render() {
@@ -22,13 +23,19 @@ class Mobile extends React.Component {
 		} else {
 			expand = 'menu-container-closed';
 		}
+		let mobile;
+		if (this.state.mobile) {
+			mobile = 'menu-right-opened';
+		} else {
+			mobile = 'menu-right-closed';
+		}
 		return (
 			<li onClick={this.clickHandler} className="top-bar-navi">
 				<a className="header-mobile-menu">Mobile</a>
-				<div className="menu-right">
+				<div className={mobile}>
 					<div className={expand}>
 						<div className="main-menu">
-							<div className='main-section'>
+							<div className="main-section">
 								<div className="list-holder">
 									<a className="menu-list-link">iOS App</a>
 									<a className="menu-list-link">Android App</a>
