708468bf214711e5e84d8a171705429c309fdeaa
Paul Ferguson <paullferguson@gmail.com>
2020-05-28 16:00:38 -0700

    Quite eslint

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const morgan = require('morgan');
+// const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
