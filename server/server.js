9c3eecd2230ba7bc07c38cdd3584dfddfb05b262
Paul Ferguson <paullferguson@gmail.com>
2020-06-02 19:23:49 -0700

    Switch frommSqualize to PG, for a 50% speed bump

--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
-// const Search = require('../database/connect-mysql');
-const Search = require('../database/connect-postgres');
+const pool = require('../database/connect-pg');
 
 const app = express();
 
@@ -14,65 +13,63 @@ app.get('/', (req, res) => {
 	res.status(200).send();
 });
 
+// Endpoints
 
+// "GetAll" now responds with a randomized ID for testing purposes
 app.get('/restaurants', (req, res) => {
-	const randPostgres = Math.floor(Math.random() * 10000000) + 1;
-	Search.findAll({
-		attributes: {
-			include: []
-		},
-		where: {
-			id: randPostgres
-		}
-		// offset: randPostgres,
-		// limit: 1,
-	})
-		.then((data) => {
-			res.status(200).send(data);
-		})
-		.catch((err) => {
-			console.log('server err from db', err);
+	const randID = Math.floor(Math.random() * 10000000) + 1;
+	const text = 'SELECT * FROM restaurants WHERE id = $1';
+	pool.connect((err, client, done) => {
+		if (err) console.log('Can not connect to the DB', err);
+		client.query(text, [randID], (error, result) => {
+			done();
+			if (error) {
+				console.log(error);
+				res.status(400).send(error);
+			}
+			res.status(200).send(result.rows);
 		});
+	});
 });
+
 
 // Add a Get-restaurant by ID for stress testing the DB
 app.get('/restaurant/:id', (req, res) => {
-	Search.findAll({
-		attributes: {
-			include: []
-		},
-		where: {
-			id: req.params.id
-		}
-	})
-		.then((data) => {
-			res.status(200).send(data);
-		})
-		.catch((err) => {
-			console.log('server err from db', err);
+	const text = 'SELECT * FROM restaurants WHERE id = $1';
+	pool.connect((err, client, done) => {
+		if (err) console.log('Can not connect to the DB', err);
+		client.query(text, [req.params.id], (error, result) => {
+			done();
+			if (error) {
+				console.log(error);
+				res.status(400).send(error);
+			}
+			res.status(200).send(result.rows);
 		});
-});
-
-app.get('/cuisines', (req, res) => {
-	console.log('/cuisines get');
-	Search.findAll({ attributes: ['cuisines'] }).then((data) => {
-		res.status(200).send(data);
 	});
 });
 
-app.get('/locations', (req, res) => {
-	console.log('/locations get');
-	Search.findAll({ attributes: ['locations'] }).then((data) => {
-		res.status(200).send(data);
-	});
-});
 
-app.get('/search', (req, res) => {
-	console.log('/search get');
-	Search.findOne({ where: { name: 'Kinjo' } }).then((data) => {
-		res.status(200);
-		res.send(data);
-	});
-});
+// app.get('/cuisines', (req, res) => {
+// 	console.log('/cuisines get');
+// 	Search.findAll({ attributes: ['cuisines'] }).then((data) => {
+// 		res.status(200).send(data);
+// 	});
+// });
+
+// app.get('/locations', (req, res) => {
+// 	console.log('/locations get');
+// 	Search.findAll({ attributes: ['locations'] }).then((data) => {
+// 		res.status(200).send(data);
+// 	});
+// });
+
+// app.get('/search', (req, res) => {
+// 	console.log('/search get');
+// 	Search.findOne({ where: { name: 'Kinjo' } }).then((data) => {
+// 		res.status(200);
+// 		res.send(data);
+// 	});
+// });
 
 module.exports = app;
